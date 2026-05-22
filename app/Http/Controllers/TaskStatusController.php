5ce5fdfd68a2d913763857ebc70577fbbc4e4efa
Irina Semyanskikh <semyanskikh@gmail.com>
2026-05-22 15:57:39 +0300

    added TaskStatusController

---
+++ app/Http/Controllers/TaskStatusController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\TaskStatus;
+
+class TaskStatusController extends Controller
+{
+    public function index()
+    {
+        $taskStatuses = TaskStatus::paginate(15);
+        return view('task_statuses.index', compact('taskStatuses'));
+    }
+
+    public function create()
+    {
+        return view('task_statuses.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|unique:task_statuses|min:1|max:255',
+        ]);
+
+        TaskStatus::create($validated);
+
+        flash()->success('Статус успешно создан');
+
+        return redirect()->route('task_statuses.index');
+    }
+
+    public function edit(TaskStatus $taskStatus)
+    {
+        return view('task_statuses.edit', compact('taskStatus'));
+    }
+
+    public function update(Request $request, TaskStatus $taskStatus)
+    {
+        $validated = $request->validate([
+            'name' => 'required|unique:task_statuses,name,' . $taskStatus->id . '|min:1|max:255',
+        ]);
+
+        $taskStatus->update($validated);
+
+        flash()->success('Статус успешно изменён');
+
+        return redirect()->route('task_statuses.index');
+    }
+
+    public function destroy(TaskStatus $taskStatus)
+    {
+        // Проверяем, есть ли задачи с этим статусом
+        if ($taskStatus->tasks()->count() > 0) {
+            // Возвращаем ошибку 403 с кастомным сообщением
+            abort(Response::HTTP_FORBIDDEN, 'This action is unauthorized.');
+        }
+
+        $taskStatus->delete();
+
+        flash()->success('Статус успешно удалён');
+
+        return redirect()->route('task_statuses.index');
+    }
+}
