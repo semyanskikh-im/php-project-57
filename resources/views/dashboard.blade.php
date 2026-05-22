8b431c1ce3af1fa0710f0a9298cd6dbc6b3c380d
Irina Semyanskikh <semyanskikh@gmail.com>
2026-05-22 16:06:18 +0300

    added view for dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,22 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+@section('content')
+<section class="bg-white dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+        <div class="mr-auto place-self-center lg:col-span-7">
+            <h1 class="max-w-2xl mb-4 text-4xl font-semibold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+                Добро пожаловать!
+            </h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                Вы успешно вошли в систему.
+            </p>
+            <div class="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+                <a href="{{ route('tasks.index') }}" 
+                   class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Перейти к задачам
+                </a>
             </div>
         </div>
     </div>
-</x-app-layout>
+</section>
+@endsection
