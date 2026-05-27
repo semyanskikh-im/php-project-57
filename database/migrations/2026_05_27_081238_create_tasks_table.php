47a96bb5cba5bfc1305f96653a7c83428364095c
Irina Semyanskikh <semyanskikh@gmail.com>
2026-05-27 11:15:06 +0300

    added model and migrations for Tasks

---
+++ database/migrations/2026_05_27_081238_create_tasks_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->foreignId('status_id')->constrained('task_statuses');
+            $table->foreignId('created_by_id')->constrained('users');
+            $table->foreignId('assigned_to_id')->nullable()->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
