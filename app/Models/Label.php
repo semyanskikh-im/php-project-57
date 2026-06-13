7287b1e9124f536f9c72980731df8be428b4eb45
Irina Semyanskikh <semyanskikh@gmail.com>
2026-06-13 11:05:05 +0300

    added model Label and migrations

---
+++ app/Models/Label.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Label extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    /**
+     * Связь с задачами (многие ко многим)
+     */
+    public function tasks()
+    {
+        return $this->belongsToMany(Task::class);
+    }
+}
---
+++ database/factories/LabelFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Label;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LabelFactory extends Factory
+{
+    protected $model = Label::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->word(),
+            'description' => $this->faker->sentence(),
+        ];
+    }
+}
