d5b2b65077774ae42305955780732536f2e785be
Irina Semyanskikh <semyanskikh@gmail.com>
2026-07-07 16:40:45 +0300

    added rail/ujs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import ujs from '@rails/ujs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Подключаем UJS для обработки data-method
+ujs.start();
